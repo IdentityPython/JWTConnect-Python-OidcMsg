bee31d2c6a3be51cc3d3727b1d2fbd132dcdf062
roland <roland@catalogix.se>
2020-03-20 16:16:00 +0100

    Release version

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Topic :: Software Development :: Libraries :: Python Modules"],
     install_requires=[
-        "cryptojwt>=0.7.16",
+        "cryptojwt>=0.7.17",
         "pyOpenSSL",
     ],
     zip_safe=False,
