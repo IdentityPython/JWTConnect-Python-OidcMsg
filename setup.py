e618096060f833be78ea5ba1994d85937336875a
roland <roland@catalogix.se>
2020-04-13 14:52:18 +0200

    Updated requirement.

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Topic :: Software Development :: Libraries :: Python Modules"],
     install_requires=[
-        "cryptojwt>=0.7.17",
+        "cryptojwt>=0.8.0",
         "pyOpenSSL",
     ],
     zip_safe=False,
