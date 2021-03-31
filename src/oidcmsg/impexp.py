4a7a408081a9234b5b284859fda86a650fa61d87
roland <roland@catalogix.se>
2021-03-31 11:19:55 +0200

    Refining dump/load implementation.

--- src/oidcmsg/impexp.py
+++ src/oidcmsg/impexp.py
@@ -1,3 +1,4 @@
+from typing import Any
 from typing import List
 from typing import Optional
 
@@ -9,12 +10,14 @@ from oidcmsg.message import Message
 
 class ImpExp:
     parameter = {}
+    special_load_dump = {}
+    init_args = []
 
     def __init__(self):
         pass
 
     def dump_attr(self, cls, item, exclude_attributes: Optional[List[str]] = None) -> dict:
-        if cls in [None, "", [], {}]:
+        if cls in [None, 0, "", [], {}, bool]:
             val = item
         elif isinstance(item, Message):
             val = {qualified_name(item.__class__): item.to_dict()}
@@ -31,7 +34,7 @@ class ImpExp:
         _exclude_attributes = exclude_attributes or []
         info = {}
         for attr, cls in self.parameter.items():
-            if attr in _exclude_attributes:
+            if attr in _exclude_attributes or attr in self.special_load_dump:
                 continue
 
             item = getattr(self, attr, None)
@@ -40,33 +43,86 @@ class ImpExp:
 
             info[attr] = self.dump_attr(cls, item, exclude_attributes)
 
+        for attr, d in self.special_load_dump.items():
+            item = getattr(self, attr, None)
+            if item:
+                info[attr] = d["dump"](item, exclude_attributes=exclude_attributes)
+
         return info
 
-    def _local_adjustments(self):
+    def local_load_adjustments(self, **kwargs):
         pass
 
-    def load_attr(self, cls, item):
-        if cls in [None, "", [], {}]:
+    def load_attr(self,
+                  cls: Any,
+                  item: dict,
+                  init_args: Optional[dict] = None,
+                  load_args: Optional[dict] = None) -> Any:
+        if load_args:
+            _kwargs = {"load_args": load_args}
+            _load_args = load_args
+        else:
+            _kwargs = {}
+            _load_args = {}
+
+        if init_args:
+            _kwargs["init_args"] = init_args
+
+        if cls in [None, 0, "", [], {}, bool]:
             val = item
         elif cls == object:
             val = importer(item)
         elif isinstance(cls, list):
-            val = [cls[0]().load(v) for v in item]
+            if isinstance(cls[0], str):
+                _cls = importer(cls[0])
+            else:
+                _cls = cls[0]
+
+            if issubclass(_cls, ImpExp) and init_args:
+                _args = {k: v for k, v in init_args.items() if k in _cls.init_args}
+            else:
+                _args = {}
+
+            val = [_cls(**_args).load(v, **_kwargs) for v in item]
         elif issubclass(cls, Message):
-            val = cls().from_dict(item)
+            _cls_name = list(item.keys())[0]
+            _cls = importer(_cls_name)
+            val = _cls().from_dict(item[_cls_name])
         else:
-            val = cls().load(item)
+            if issubclass(cls, ImpExp) and init_args:
+                _args = {k: v for k, v in init_args.items() if k in cls.init_args}
+            else:
+                _args = {}
+
+            val = cls(**_args).load(item, **_kwargs)
 
         return val
 
-    def load(self, item: dict):
+    def load(self, item: dict,
+             init_args: Optional[dict] = None,
+             load_args: Optional[dict] = None):
+
+        if load_args:
+            _kwargs = {"load_args": load_args}
+            _load_args = load_args
+        else:
+            _kwargs = {}
+            _load_args = {}
+
+        if init_args:
+            _kwargs["init_args"] = init_args
+
         for attr, cls in self.parameter.items():
-            if attr not in item:
+            if attr not in item or attr in self.special_load_dump:
                 continue
 
-            setattr(self, attr, self.load_attr(cls, item[attr]))
+            setattr(self, attr, self.load_attr(cls, item[attr], **_kwargs))
 
-        self._local_adjustments()
+        for attr, func in self.special_load_dump.items():
+            if attr in item:
+                setattr(self, attr, func["load"](item[attr], **_kwargs))
+
+        self.local_load_adjustments(**_load_args)
         return self
 
     def flush(self):
@@ -78,6 +134,10 @@ class ImpExp:
         for attr, cls in self.parameter.items():
             if cls is None:
                 setattr(self, attr, None)
+            elif cls == 0:
+                setattr(self, attr, 0)
+            elif cls is bool:
+                setattr(self, attr, False)
             elif cls == "":
                 setattr(self, attr, "")
             elif cls == []:
--- src/oidcmsg/item.py
+++ src/oidcmsg/item.py
@@ -2,19 +2,19 @@ from typing import List
 from typing import Optional
 
 from oidcmsg.impexp import ImpExp
+from oidcmsg.message import Message
 from oidcmsg.storage import importer
 from oidcmsg.storage.utils import qualified_name
 
 
 class DLDict(ImpExp):
     parameter = {
-        "item_class": "",
         "db": {}
     }
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         ImpExp.__init__(self)
-        self.db = {}
+        self.db = kwargs
 
     def __setitem__(self, key: str, val):
         self.db[key] = val
@@ -22,18 +22,46 @@ class DLDict(ImpExp):
     def __getitem__(self, key: str):
         return self.db[key]
 
+    def __delitem__(self, key:str):
+        del self.db[key]
+
     def dump(self, exclude_attributes: Optional[List[str]] = None) -> dict:
         res = {}
 
-        for k,v in self.db.items():
+        for k, v in self.db.items():
             _class = qualified_name(v.__class__)
             res[k] = [_class, v.dump(exclude_attributes=exclude_attributes)]
 
         return res
 
-    def load(self, spec: dict, **kwargs) -> "DLDict":
+    def load(self, spec: dict,
+             init_args: Optional[dict] = None,
+             load_args: Optional[dict] = None
+             ) -> "DLDict":
+        if load_args:
+            _kwargs= {"load_args": load_args}
+            _load_args = {}
+        else:
+            _load_args = {}
+            _kwargs = {}
+
+        if init_args:
+            _kwargs["init_args"] = init_args
+
         for attr, (_item_cls, _item) in spec.items():
-            self.db[attr] = importer(_item_cls)(**kwargs).load(_item)
+            _cls = importer(_item_cls)
+
+            if issubclass(_cls, ImpExp) and init_args:
+                _args = {k: v for k, v in init_args.items() if k in _cls.init_args}
+            else:
+                _args = {}
+
+            _x = _cls(**_args)
+            _x.load(_item, **_kwargs)
+            self.db[attr] = _x
+
+        self.local_load_adjustments(**_load_args)
+
         return self
 
     def keys(self):
@@ -47,3 +75,63 @@ class DLDict(ImpExp):
 
     def __contains__(self, item):
         return item in self.db
+
+    def get(self, item, default=None):
+        return self.db.get(item, default)
+
+    def __len__(self):
+        return len(self.db)
+
+
+def dump_dldict(item, exclude_attributes: Optional[List[str]] = None) -> dict:
+    res = {}
+
+    for k, v in item.items():
+        _class = qualified_name(v.__class__)
+        if isinstance(v, Message):
+            res[k] = [_class, v.to_dict()]
+        else:
+            res[k] = [_class, v.dump(exclude_attributes=exclude_attributes)]
+
+    return res
+
+
+def load_dldict(spec: dict,
+                init_args: Optional[dict] = None,
+                load_args: Optional[dict] = None
+                ) -> dict:
+    db = {}
+
+    for attr, (_item_cls, _item) in spec.items():
+        _class = importer(_item_cls)
+        if issubclass(_class, Message):
+            db[attr] = _class().from_dict(_item)
+        else:
+            if issubclass(_class, ImpExp) and init_args:
+                _args = {k: v for k, v in init_args.items() if k in _class.init_args}
+            else:
+                _args = {}
+
+            db[attr] = _class(**_args).load(_item)
+
+    return db
+
+
+def dump_class_map(item, exclude_attributes: Optional[List[str]] = None) -> dict:
+    _dump = {}
+    for key, val in item.items():
+        if isinstance(val, str):
+            _dump[key] = val
+        else:
+            _dump[key] = qualified_name(val)
+    return _dump
+
+
+def load_class_map(spec: dict,
+                   init_args: Optional[dict] = None,
+                   load_args: Optional[dict] = None
+                   ) -> dict:
+    _item = {}
+    for key, val in spec.items():
+            _item[key] = importer(val)
+    return _item
